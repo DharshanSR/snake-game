ccc97383e6ce241e109145096e5193591fd1c63b
Ravindran Dharshan <dharshanravindran8@gmail.com>
2025-08-18 22:58:45 +0530

    Add Obstacles class for snake game with obstacle generation and collision detection

---
+++ obstacles.py
@@ -0,0 +1,33 @@
+import random
+
+OBSTACLE_COLOR = (128, 0, 128)
+OBSTACLE_COUNT = 7
+
+class Obstacles:
+    def __init__(self, screen_width, screen_height, snake_block, snake_start, food_pos):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.snake_block = snake_block
+        self.snake_start = snake_start
+        self.food_pos = food_pos
+        self.obstacles = self.generate_obstacles()
+
+    def generate_obstacles(self):
+        obstacles = []
+        while len(obstacles) < OBSTACLE_COUNT:
+            ox = round(random.randrange(0, self.screen_width - self.snake_block) / 10.0) * 10.0
+            oy = round(random.randrange(0, self.screen_height - self.snake_block) / 10.0) * 10.0
+            if [ox, oy] == self.snake_start or [ox, oy] == self.food_pos:
+                continue
+            if [ox, oy] in obstacles:
+                continue
+            obstacles.append([ox, oy])
+        return obstacles
+
+    def draw(self, screen):
+        for obs in self.obstacles:
+            import pygame
+            pygame.draw.rect(screen, OBSTACLE_COLOR, [obs[0], obs[1], self.snake_block, self.snake_block])
+
+    def is_collision(self, pos):
+        return pos in self.obstacles
